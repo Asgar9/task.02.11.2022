e48b64c42cd29bde60a5b12e17a7e70594126a9b
Asgar9 <109238750+Asgar9@users.noreply.github.com>
2022-11-02 11:23:18 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,4 +7,7 @@ $('#tabs-nav li').click(function(){
   $('#tabs-nav li').removeClass('active');
   $(this).addClass('active');
   $('.tab-content').hide();
+  var activeTab = $(this).find('a').attr('href');
+  $(activeTab).fadeIn();
+  return false;
   });
